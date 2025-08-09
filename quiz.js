87d6395f74fc66f89b0c285cc102e9171f6b8f6c
Abigail Owusu Acheampong <owusuacheamponga@gmail.com>
2025-08-09 21:36:10 +0000

    made changes to the codes because it got struck in the incorrect, try again loop

--- quiz.js
+++ quiz.js
@@ -4,7 +4,11 @@ document.addEventListener('DOMContentLoaded', function() {
  function checkAnswer(){
   // This function checks if the user's selected answer is correct
   const correctAnswer = "4"; // The correct answer for the quiz question
-  const userAnswer = document.querySelector('input[name="quiz"]:checked'); //Selects the checked radio button with name="quiz"
+  const selectedOption = document.querySelector('input[name="quiz"]:checked'); //Selects the checked radio button with name="quiz"
+  let userAnswer = ""; //Initializes userAnswer to an empty string 
+  if (selectedOption) {
+   userAnswer = selectedOption.value; // If an option is selected, get its value
+  }
   const feedback = document.getElementById('feedback'); // a variable for storing the message of whether correct or wrong
   if (userAnswer == correctAnswer) {
    feedback.textContent = 'Correct! Well done.'; // Displays a success message
